0eea9a1c937180d753e71ed7757cba8170e05240
Shilcof <robert.shilcof@gmail.com>
2021-02-01 11:21:21 +0000

    commit tree 95224a28d7b7cddddcb530a5555c2461b5ba2253

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+function superbowlWin(bowls) { 
+  const win = bowls.find(bowl => bowl.result === "W");
+  return win ? win.year : win;
+}
